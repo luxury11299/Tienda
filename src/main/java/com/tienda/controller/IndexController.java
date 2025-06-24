29c304ba84cc9e3e38d327dbfae5579718a77cf4
luxury11299 <kevin11299@hotmail.com>
2025-06-24 15:09:07 -0600

    Finalizacion semana 04

---
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.tienda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    
+    @RequestMapping("/")//url al que responde
+    public String index() {
+        return "index"; //nombre de la vista en templates a mostrar
+    }
+
+    
+    @RequestMapping("/info")//url al que responde
+    public String info() {
+        return "info"; //nombre de la vista en templates a mostrar
+    }
+    
+}
