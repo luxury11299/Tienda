e20cf66d403b18e09a9777ec90a084027287ffad
luxury11299 <kevin11299@hotmail.com>
2025-07-15 09:45:02 -0600

    semana 6

--- src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -1,4 +1,3 @@
-
 package com.tienda.service.impl;
 
 import com.tienda.dao.CategoriaDao;
@@ -7,27 +6,42 @@ import com.tienda.service.CategoriaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 
 //Define la logica de los metodos de la interfaz que implementa
 @Service
-public class CategoriaServiceImpl implements CategoriaService{
-    
+public class CategoriaServiceImpl implements CategoriaService {
+
     //inyeccion
     @Autowired
     private CategoriaDao categoriaDao;
 
     @Override
+    @Transactional(readOnly = true)
     public List<Categoria> getCategorias(boolean activos) {
-        List<Categoria> lista = categoriaDao.findAll();
-        
-        //Filtrar solo los activos
-        if (activos){
-            lista.removeIf(cat -> !cat.isActivo());
+        List <Categoria> lista = categoriaDao.findAll();
+        if (activos) {
+            lista.removeIf(e -> !e.isActivo());
         }
-        
         return lista;
     }
-    
-    
+
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.delete(categoria);
+    }
+
 }
